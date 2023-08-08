2bb5b87641421a7c47e6e5cbf95d6c2f149e86d2
Polina Khvorostukhina <polinakhvorost@yandex.ru>
2023-08-08 18:17:43 +0300

    hotfix: добавить в куки secure

--- controllers/users.js
+++ controllers/users.js
@@ -21,6 +21,7 @@ const login = (req, res, next) => {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true,
         sameSite: 'none',
+        secure: true,
       })
         .send(user);
     })
